5e46071337f9602f998416262387e9535d8d271b
MyUbuntu <yz2297@cornell.edu>
2020-05-04 20:50:03 -0700

    debug

--- ath_validation/openwhisk-harv-vm-cgroup-ath/core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/ContainerProxy.scala
+++ ath_validation/openwhisk-harv-vm-cgroup-ath/core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/ContainerProxy.scala
@@ -697,7 +697,7 @@ class ContainerProxy(
     val sendResult = if (job.msg.blocking) {
       activation.map(
         // yanqi, cpu_util & exe time is included in completion ack, not result ack
-        sendActiveAck(tid, _, job.msg.blocking, job.msg.rootControllerIndex, job.msg.user.namespace.uuid, false, 0.0))
+        sendActiveAck(tid, _, job.msg.blocking, job.msg.rootControllerIndex, job.msg.user.namespace.uuid, false, 0.0, 0))
     } else {
       // For non-blocking request, do not forward the result.
       Future.successful(())
