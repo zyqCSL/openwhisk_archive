33ff20ca50523797311114c6f3d002c03a631872
MyUbuntu <yz2297@cornell.edu>
2020-05-07 05:52:42 -0700

    change to ath-5

--- ath_validation/openwhisk-harv-vm-cgroup-ath/ansible/setup_couchdb.sh
+++ ath_validation/openwhisk-harv-vm-cgroup-ath/ansible/setup_couchdb.sh
@@ -2,6 +2,6 @@ export OW_DB=CouchDB
 export OW_DB_USERNAME=yz2297
 export OW_DB_PASSWORD=openwhisk_couch
 export OW_DB_PROTOCOL=http
-export OW_DB_HOST=128.253.128.76
+export OW_DB_HOST=128.253.128.68
 export OW_DB_PORT=5984
 ansible-playbook setup.yml
