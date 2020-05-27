32bef1f6ce668488feed30ac7774c2074384235d
MyUbuntu <yz2297@cornell.edu>
2020-05-27 10:36:06 -0700

    debug csv

--- ath_validation/openwhisk_locust/test_image_proc.sh
+++ ath_validation/openwhisk_locust/test_image_proc.sh
@@ -1,4 +1,4 @@
-docker run -v $PWD:/mnt/locust locustio/locust -f /mnt/locust/general_locust_file.py \
+docker run -v $PWD:/mnt/locust -f /mnt/locust/general_locust_file.py \
 	--csv=/mnt/locust/image_process --headless -t 3m \
 	--host https://172.17.0.1 --users 1 \
-	--tags image_process
+	--tags image_process locustio/locust
