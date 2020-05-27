5056369e84c7c33ba9b4af03d61508f793e0396c
MyUbuntu <yz2297@cornell.edu>
2020-05-27 10:00:56 -0700

    add locust

--- ath_validation/openwhisk_locust/test_image_proc.sh
+++ ath_validation/openwhisk_locust/test_image_proc.sh
@@ -1,4 +1,4 @@
-docker run -v $PWD:/mnt/locust locustio/locust -f /mnt/locust/locustfile.py \
+docker run -v $PWD:/mnt/locust locustio/locust -f /mnt/locust/general_locust_file.py \
 	--csv=image_process --headless -t 3m \
 	--host https://172.17.0.1 --users 1 \
 	--tags image_process
