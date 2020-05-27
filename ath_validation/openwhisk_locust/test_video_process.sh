465e863668279d5394e78897db15a3f792ac497f
MyUbuntu <yz2297@cornell.edu>
2020-05-27 10:31:52 -0700

    debug csv

--- ath_validation/openwhisk_locust/test_video_process.sh
+++ ath_validation/openwhisk_locust/test_video_process.sh
@@ -1,4 +1,4 @@
 docker run -v $PWD:/mnt/locust locustio/locust -f /mnt/locust/general_locust_file.py \
-	--csv=./video_process --headless -t 3m \
+	--csv=/mnt/locust/video_process --headless -t 3m \
 	--host https://172.17.0.1 --users 1 \
 	--tags video_process
