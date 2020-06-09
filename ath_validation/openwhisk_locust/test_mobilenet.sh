950962641e5a5f7bf1fcfb83c36e0d512d4b947d
MyUbuntu <yz2297@cornell.edu>
2020-06-09 10:12:04 -0700

    debug

--- ath_validation/openwhisk_locust/test_mobilenet.sh
+++ ath_validation/openwhisk_locust/test_mobilenet.sh
@@ -1,5 +1,5 @@
 docker run -v $PWD:/mnt/locust -v $HOME/openwhisk_locust_log:/mnt/locust_log yz2297/locust_openwhisk \
 	-f /mnt/locust/general_locust_file.py \
-	--csv=/mnt/locust/mobilenet --headless -t $1 \
+	--csv=/mnt/locust_log/mobilenet --headless -t $1 \
 	--host https://172.17.0.1 --users $2 \
 	--tags mobilenet
