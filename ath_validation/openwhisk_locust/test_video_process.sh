5837b3a76faa6775afa82b624fee51d0fefd791f
MyUbuntu <yz2297@cornell.edu>
2020-06-09 08:40:49 -0700

    delete docker file

--- ath_validation/openwhisk_locust/test_video_process.sh
+++ ath_validation/openwhisk_locust/test_video_process.sh
@@ -1,5 +1,5 @@
 docker run -v $PWD:/mnt/locust -v $HOME/openwhisk_locust_log:/mnt/locust_log yz2297/locust_openwhisk \
 	-f /mnt/locust/general_locust_file.py \
-	--csv=/mnt/locust/video_process --headless -t 3m \
-	--host https://172.17.0.1 --users 10 \
+	--csv=/mnt/locust/video_process --headless -t $1 \
+	--host https://172.17.0.1 --users $2 \
 	--tags video_process
