0dea846d4fc17bd7cb57b9c70f9fbdb4f0486e63
MyUbuntu <yz2297@cornell.edu>
2020-08-24 02:46:00 -0700

    change on workload

--- ath_validation/openwhisk_locust/profile_fbench.sh
+++ ath_validation/openwhisk_locust/profile_fbench.sh
@@ -1,5 +1,5 @@
 python3 ./profile_function.py --min-users 1 --max-users 5 --user-step 1 --profile-users 4 --function video_process --iat 1
 python3 ./profile_function.py --min-users 10 --max-users 40 --user-step 5 --profile-users 10 --function image_process --iat 1
 python3 ./profile_function.py --min-users 2 --max-users 12 --user-step 2 --profile-users 4 --function mobilenet --iat 1
-python3 ./profile_function.py --min-users 2 --max-users 10 --user-step 1 --profile-users 2 --function linpack --iat 1
-python3 ./profile_function.py --min-users 2 --max-users 10 --user-step 2 --profile-users 4 --function matmult --iat 1
+python3 ./profile_function.py --min-users 1 --max-users 7 --user-step 1 --profile-users 2 --function linpack --iat 1
+python3 ./profile_function.py --min-users 5 --max-users 30 --user-step 5 --profile-users 10 --function matmult --iat 1
