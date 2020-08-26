2b52d1150eb6e1f5856f9708091a3a46ffb76e63
MyUbuntu <yz2297@cornell.edu>
2020-08-26 06:23:20 -0700

    reprofile

--- ath_validation/openwhisk_locust/profile_fbench.sh
+++ ath_validation/openwhisk_locust/profile_fbench.sh
@@ -1,4 +1,4 @@
-python3 ./profile_function.py --min-users 1 --max-users 5 --user-step 1 --profile-users 4 --function video_process --iat 1
+python3 ./profile_function.py --min-users 1 --max-users 5 --user-step 1 --profile-users 2 --function video_process --iat 1
 python3 ./profile_function.py --min-users 5 --max-users 40 --user-step 5 --profile-users 10 --function image_process --iat 1
 python3 ./profile_function.py --min-users 2 --max-users 12 --user-step 2 --profile-users 4 --function mobilenet --iat 1
 python3 ./profile_function.py --min-users 1 --max-users 7 --user-step 1 --profile-users 2 --function linpack --iat 1
