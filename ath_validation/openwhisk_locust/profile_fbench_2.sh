5d93bdacf830eac2484ee908c8d6f9476db85c94
MyUbuntu <yz2297@cornell.edu>
2020-08-27 05:15:29 -0700

    debug

--- ath_validation/openwhisk_locust/profile_fbench_2.sh
+++ ath_validation/openwhisk_locust/profile_fbench_2.sh
@@ -1,3 +1,1 @@
-# python3 ./profile_function.py --min-users 5 --max-users 20 --user-step 5 --profile-users 5 --function linpack --iat 1
-# python3 ./profile_function.py --min-users 2 --max-users 10 --user-step 2 --profile-users 4 --function matmult --iat 1
 python3 ./profile_function.py --min-users 2 --max-users 10 --user-step 2 --profile-users 4 --function chameleon --iat 1
