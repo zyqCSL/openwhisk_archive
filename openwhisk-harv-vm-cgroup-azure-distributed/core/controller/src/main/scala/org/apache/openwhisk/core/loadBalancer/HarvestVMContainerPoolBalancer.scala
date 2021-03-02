a09684fecbe1002e909e4d98bd656d4808e267ec
yanqi <yz2297@cornell.edu>
2021-03-02 20:11:37 +0800

    fix typo

--- openwhisk-harv-vm-cgroup-azure-distributed/core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/HarvestVMContainerPoolBalancer.scala
+++ openwhisk-harv-vm-cgroup-azure-distributed/core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/HarvestVMContainerPoolBalancer.scala
@@ -1093,20 +1093,20 @@ case class HarvestVMContainerPoolBalancerState(
 
         // val newInvokerDetails = onlyNewInvokers
         // record info of non-offline invokers for rsc change
-        val invokerDetails = _invokers.filter(_.status.toString != "down")
+        val invokerDetails = _invokers.filter(_.status.asString != "down")
           .map(i =>
             s"${i.id.toString}: ${i.status} avail cpu(cores) ${i.cpu} mem ${i.memory}MB")
           .mkString(", ")
         s"number of known invokers increased: new = $newSize, old = $oldSize. details: $invokerDetails."
       } else {
-        val invokerDetails = _invokers.filter(_.status.toString != "down")
+        val invokerDetails = _invokers.filter(_.status.asString != "down")
           .map(i =>
             s"${i.id.toString}: ${i.status} avail cpu(cores) ${i.cpu} mem ${i.memory}MB")
           .mkString(", ")
         s"number of known invokers decreased: new = $newSize, old = $oldSize. details: $invokerDetails."
       }
     } else {
-      val invokerDetails = _invokers.filter(_.status.toString != "down")
+      val invokerDetails = _invokers.filter(_.status.asString != "down")
           .map(i =>
             s"${i.id.toString}: ${i.status} avail cpu(cores) ${i.cpu} mem ${i.memory}MB")
           .mkString(", ")
