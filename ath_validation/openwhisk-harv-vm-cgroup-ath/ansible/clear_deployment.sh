e965888b2dcc50905b4c616d8c486573b231754e
yanqi <yz2297@cornell.edu>
2021-04-03 22:25:53 +0800

    remove stale versions

--- ath_validation/openwhisk-harv-vm-cgroup-ath/ansible/clear_deployment.sh
+++
@@ -1,4 +0,0 @@
-ansible-playbook -i environments/local couchdb.yml -e mode=clean
-ansible-playbook -i environments/local openwhisk.yml -e mode=clean
-ansible-playbook -i environments/local postdeploy.yml -e mode=clean
-ansible-playbook -i environments/local apigateway.yml -e mode=clean
